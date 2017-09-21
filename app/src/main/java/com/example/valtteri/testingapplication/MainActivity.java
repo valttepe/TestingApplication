0434318641734507e88540132f69cafb66fe1797
valttepe <valtteri.pesu@hotmail.com>
2017-09-21 21:11:06 +0300

    Initial commit

---
+++ app/src/main/java/com/example/valtteri/testingapplication/MainActivity.java
@@ -0,0 +1,110 @@
+package com.example.valtteri.testingapplication;
+
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.mbientlab.metawear.Data;
+import com.mbientlab.metawear.MetaWearBoard;
+import com.mbientlab.metawear.Route;
+import com.mbientlab.metawear.Subscriber;
+import com.mbientlab.metawear.android.BtleService;
+import com.mbientlab.metawear.builder.RouteBuilder;
+import com.mbientlab.metawear.builder.RouteComponent;
+import com.mbientlab.metawear.data.MagneticField;
+import com.mbientlab.metawear.module.MagnetometerBmm150;
+
+import bolts.Continuation;
+import bolts.Task;
+
+public class MainActivity extends AppCompatActivity implements ServiceConnection {
+    private final String MW_MAC_ADDRESS = "CB:AA:89:01:48:20";
+    private BtleService.LocalBinder serviceBinder;
+    private MetaWearBoard board;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Bind the service when the activity is created
+        getApplicationContext().bindService(new Intent(this, BtleService.class),
+                this, Context.BIND_AUTO_CREATE);
+
+
+    }
+
+
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        board.tearDown();
+        getApplicationContext().unbindService(this);
+    }
+
+    @Override
+    public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+        // Typecast the binder to the service's LocalBinder class
+        serviceBinder = (BtleService.LocalBinder) iBinder;
+        retrieveBoard();
+
+        board.connectAsync().onSuccessTask(new Continuation<Void, Task<Void>>() {
+            @Override
+            public Task<Void> then(Task<Void> task) throws Exception {
+                final MagnetometerBmm150 magnetometer = board.getModule(MagnetometerBmm150.class);
+                // use the regular preset configuration
+                magnetometer.usePreset(MagnetometerBmm150.Preset.REGULAR);
+                return magnetometer.magneticField().addRouteAsync(new RouteBuilder() {
+                    @Override
+                    public void configure(RouteComponent source) {
+                        source.stream(new Subscriber() {
+                            @Override
+                            public void apply(Data data, Object ... env) {
+                                Log.i("MainActivity", data.value(MagneticField.class).toString());
+                            }
+                        });
+                    }
+                }).continueWith(new Continuation<Route, Void>() {
+                    @Override
+                    public Void then(Task<Route> task) throws Exception {
+                        magnetometer.magneticField().start();
+                        magnetometer.start();
+                        return null;
+                    }
+                });
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName componentName) {
+
+    }
+
+    public void retrieveBoard() {
+        final BluetoothManager btManager=
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        final BluetoothDevice remoteDevice=
+                btManager.getAdapter().getRemoteDevice(MW_MAC_ADDRESS);
+
+        // Create a MetaWear board object for the Bluetooth Device
+        board= serviceBinder.getMetaWearBoard(remoteDevice);
+    }
+
+    
+
+}
+
